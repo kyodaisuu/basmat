48f4d78d6be09796f316a4855955a916f5777482
Fish <37192601+kyodaisuu@users.noreply.github.com>
2018-06-27 18:24:26 +0900

    Merged Koteitan's version (v2.1 and 2.2)
    https://github.com/koteitan/basmat-2.x/blob/master/basmat.c
    Updated on Mar 16, 2018

--- basmat.c
+++ basmat.c
@@ -1,6 +1,6 @@
 /*
 
-basmat - Bashicu Matrix Calculator version 2.0
+basmat - Bashicu Matrix Calculator version 2.1
 
 Usage: basmat [-v ver] [-o opt] [-s seq] [-t stp] [-dh] ini
 
@@ -8,9 +8,10 @@ Usage: basmat [-v ver] [-o opt] [-s seq] [-t stp] [-dh] ini
        BM: sequence expression of Bashicu matrix.
        n: natural number. n=2 if not given.
        ex. "(0,0)(1,1)[3]"   (Quote to escape brackets in shell)
-  ver: Version of Bashicu matrix system. Default = 1.
-       ver = 1: The first simple version in 2014.
-       ver = 2: Modified version to avoid non-terminating pattern in 2016.
+  ver: Version of Bashicu matrix system. Default = 2.1.
+       ver = 1.0 or 1: The first simple version in 2014.
+       ver = 2.0 or 2: Modified version to avoid non-terminating pattern in 2016.
+       ver = 2.1     : Modified version to fit Buchholz's Hydra.
   opt: Calculation option.
        opt = 1: n is constant. (Default)
        opt = 2: n = n+1 for each loop.
@@ -27,14 +28,22 @@ Example:
      basmat -o 4 -s 100000 "(0,0)(1,1)[3]"
      basmat -o 1 -s 13000 -t 100000 "(0,0)(1,1)[3]"
 
-Web interface is available at
-     http://gyafun.jp/ln/basmat.cgi
-
-Author: Fish
-     http://googology.wikia.com/wiki/User:Kyodaisuu
-     
-Version History
+Version History:
+     Version 1.0 (SEP  1 2014): Algorithm by Bashicu,  Program by Fish
+     Version 2.0 (APR 14 2017): Algorithm by Bashicu,  Program by Fish
+     Version 2.1 (MAR    2018): Algorithm by koteitan, Program by koteitan
+     Version 2.2 (MAR    2018): Algorithm by koteitan, Program by koteitan
+     http://ja.googology.wikia.com/wiki/ユーザーブログ:BashicuHyudora/BASIC言語による巨大数のまとめ#.E3.83.90.E3.82.B7.E3.82.AF.E8.A1.8C.E5.88.97.E6.95.B0.28Bashicu_matrix_number.29
+     https://github.com/koteitan/basmat-2.1
      http://gyafun.jp/ln/basmat/ChangeLog.txt
+
+Authors:
+  Bashicu
+    http://ja.googology.wikia.com/wiki/ユーザーブログ:BashicuHyudora
+  Fish
+    http://googology.wikia.com/wiki/User:Kyodaisuu
+  koteitan
+    https://twitter.com/koteitan
 
 ***********************************/
 
@@ -44,21 +53,89 @@ Version History
 #include <string.h>
 #include <unistd.h>
 
+void printM(int *A, int nr, int nc){
+  int r,c;
+  for(c=0;c<nc;c++){
+    printf("(");
+    for(r=0;r<nr-1;r++)printf("%d,",A[r+c*nr]);
+    if(nr>0)printf("%d)",A[nr-1+c*nr]);
+  }
+}
+void printA(int *A, int nr){
+  int r=0;
+  printf("(");
+  for(r=0;r<nr-1;r++)printf("%d,",A[r]);
+  if(nr>0)printf("%d)",A[nr-1]);
+}
+/* returns index of the element x in P with nr rows (it returns -1 when x is not found) */
+int find(int *P, int nr, int x){
+  int i;
+  for(i=0;i<nr;i++) if(P[i]==x) return i;
+  return -1;
+}
+/* are the all elements equal in nr rows of P */
+int areAllEq(int *P, int nr){
+  int i, p0=P[0];
+  for(i=1;i<nr;i++) if(P[i]!=p0) return 0;
+  return -1;  
+}
+/* argmin returns the index whose element is minimum of P in nr rows */
+int argmax(int *P, int nr){
+  int i, m=nr;
+  for(i=0;i<nr;i++) if(P[i]>P[m]) m=i;
+  return m;
+}
+/* getParent returns the column number of the parent of the child column c in row r in Bashicu matrix S consist of the nr rows (it returns -1 when parent is not found) */
+int getParent(int *S, int r, int c, int nr){
+  int i;
+  for(i=c;i>=0;i--) if(S[r+i*nr]<S[r+c*nr]) return i;
+  return -1; 
+}
+/* getConcestor returns the column number of the concestor of with m rows from the child column c in Bashicu matrix S consist of the nr rows (it returns -1 when concestor is not found) */
+int getConcestor(int *S, int m, int c, int nr){
+  int r, maxr;
+  int *P = malloc(sizeof(int) * nr);
+  
+  for(r=0;r<m;r++) P[r] = getParent(S,r,c,nr);
+  while(find(P,m,-1)==-1){
+    if(areAllEq(P,m)){
+      /* same */
+      r = P[0];
+      free(P);
+      return r;
+    }else{
+      /* not same */
+      maxr = argmax(P,m);
+      P[maxr]=getParent(S,maxr,P[maxr],nr);
+    }
+  }
+  free(P);
+  return -1;
+}
+
 int main(int argc, char *argv[]) {
   /**************************** Initialization ****************************/
-  int *S, *Delta, *C, row=0, bad, found, i=0, j, l, m, nr, len;
-  int opt=1, ver=1, detail=0, help=0;
+  int *S, *Delta, *C, *P, row=0, bad, found, i=0, j, l, m, nr, len;
+  int opt=1, ver=210, detail=0, help=0;
   long n, nn, k, num=0, s=20, step=0, maxstep=0;
   char *bm, arg;
-
   /* Read commandline options */
 
  while ((arg = getopt(argc, argv, "v:o:s:t:dh")) != -1) {
     switch (arg) {
-        case 'v': ver=atoi(optarg); break;
+        case 'v': 
+          if     (strcmp(optarg,"1.0")==0||strcmp(optarg,"1")==0)ver=100;
+          else if(strcmp(optarg,"2.0")==0||strcmp(optarg,"2")==0)ver=200;
+          else if(strcmp(optarg,"2.1")==0                       )ver=210;
+          else if(strcmp(optarg,"2.2")==0                       )ver=220;
+          else{
+            printf("Error: invalid version.\n");
+            help=1;
+          }
+        break;
         case 'o': opt=atoi(optarg); break;
         case 's': s=atoi(optarg); break;
-        case 't':  maxstep=atoi(optarg); break;
+        case 't': maxstep=atoi(optarg); break;
         case 'd': detail=1; break;
         case 'h': help=1;
         default: /* '?' */
@@ -66,7 +143,6 @@ int main(int argc, char *argv[]) {
     }
   }
   
-  if (ver < 1 || ver > 2) {printf("Error: invalid version.\n"); help=1;}
   if (opt < 1 || opt > 4) {printf("Error: invalid opt value.\n"); help=1;}
   if (s<0) {printf("Error: Negative value of seq not allowed.\n"); help=1;}
   if (maxstep<0) {printf("Error: Negative value of stp not allowed.\n"); help=1;}
@@ -95,9 +171,9 @@ int main(int argc, char *argv[]) {
   nr = row+1;
 
   /* Allocate array */
-  S = malloc(sizeof(int) * nr * s);
+  S     = malloc(sizeof(int) * nr * s);
   Delta = malloc(sizeof(int) * nr);
-  C = malloc(sizeof(int) * nr * s);
+  C     = malloc(sizeof(int) * nr * s);
   if (S == NULL || Delta == NULL || C == NULL) {
     printf("Allocation failed.\n");
     exit(-1);
@@ -129,9 +205,10 @@ int main(int argc, char *argv[]) {
     "       BM sequence expression of Bashicu matrix.\n"
     "       n: natural number. n=2 if not given.\n"
     "       ex. \"(0,0)(1,1)[3]\"   (Quote to escape brackets in shell)\n"
-    "  ver  Version of Bashicu matrix system (1 or 2). Default = 1.\n"
-    "       ver = 1: The first simple version in 2014.\n"
-    "       ver = 2: Modified version to avoid non-terminating pattern in 2016.\n"
+    "  ver  Version of Bashicu matrix system (1 or 2). Default = 2.1.\n"
+    "       ver = 1 or 1.0: The first simple version in 2014.\n"
+    "       ver = 2 or 2.0: Modified version to avoid non-terminating pattern in 2016.\n"
+    "       ver = 2.1     : Modified version to fit Buchholz's Hydra.\n"
     "  opt  Calculation option.\n"
     "       opt = 1: n is constant. (Default)\n"
     "       opt = 2: n = n+1 for each loop.\n"
@@ -152,7 +229,7 @@ int main(int argc, char *argv[]) {
 
   /**************************** Start calculation ****************************/
   for (n = j-1; n >= 0; n--) {
-
+    if(detail)printf("--------------------------------------------\n");
     /* Show current sequence */
     for (i=0; i<=n; i++) {
       printf ("(");
@@ -172,7 +249,7 @@ int main(int argc, char *argv[]) {
       /* If the first row of S_n is 0, no bad sequence */
       if (opt == 4) num++;
     } else {
-      if (ver == 1) {
+      if (ver == 100) {
         /***** Version 1 *****/
         /* Calculate m = Uppermost zero at the rightmost sequence */
         m = row+1; /* row = number of rows + 1 */
@@ -188,13 +265,23 @@ int main(int argc, char *argv[]) {
             bad = k; k = n+1;
           }
         }
-      } else {
+      } else if(ver == 200){
         /***** Version 2 *****/
         /* Clear Delta */
         for (m=0; m<=row; m++) Delta[m]=0;
         /* Determine the bad sequence and calculate Delta */
-        for (k=0; k<=n; k++) {
-          for (l = 0; l <= row; l++) {
+        for (k=0; k<=n; k++) { /* k = pivot column */
+          if(detail){
+            printf ("S[n]-D= (");
+            for (l=0; l<row; l++) printf ("%d,",S[l+n*nr]-Delta[l]);
+            printf ("%d)\n", S[row+n*nr]-Delta[row]);
+          }
+          if(detail){
+            printf ("S[n-k]= (");
+            for (l=0; l<row; l++) printf ("%d,",S[l+(n-k)*nr]);
+            printf ("%d)\n", S[row+(n-k)*nr]);
+          }
+          for (l = 0; l <= row; l++) { /* l = row */
              if (S[l + (n-k)*nr] < S[l + n*nr] - Delta[l]) {
                if (S[l+1 + n*nr] == 0 || l==row) {
                  l = row; bad=k; k=n;
@@ -204,6 +291,11 @@ int main(int argc, char *argv[]) {
              } else {
                l = row; /* Go to left sequence (k loop) */
              }
+          }
+          if(detail){
+            printf ("Delta = (");
+            for (l=0; l<row; l++) printf ("%d,",Delta[l]);
+            printf ("%d)\n", Delta[row]);
           }
         }
         /* Calculate C matrix */
@@ -217,6 +309,90 @@ int main(int argc, char *argv[]) {
                  l=0;
               }
            }
+        }
+        if(detail){
+            printf ("C     = ");
+            for (l=1; l<=bad; l++) {
+                printf ("(");
+                for (m=0; m<row; m++) printf ("%d,",C[m+l*nr]);
+                printf ("%d)",C[row+l*nr]);
+            }
+            printf ("\n");
+        }
+      }else if(ver == 210){
+        /***** Version 2.1 *****/
+        /* Calculate m = Uppermost zero at the rightmost sequence */
+        m = row+1; /* row = number of rows + 1 */
+        for (j=0; j<=row; j++){
+          if (S[j + n*nr] == 0) m=j, j=row+1;
+        }
+        /* Determine the bad sequence and calculate Delta */
+        for (k=0; k<=n; k++) {
+          /* find and jump to parent of n */
+          for (l=1; n-(k+l)>=0; l++) {
+            if(S[0+(n-(k+l))*nr] < S[0+(n-k)*nr]){
+#if 0
+              printf("find tree parent:\n");
+              printf("child =(");
+              for (j=0; j<row; j++) printf ("%d,",S[j+(n-k)*nr]);
+              printf ("%d)\n", S[j+(n-k)*nr]);
+              printf("parent=(");
+              for (j=0; j<row; j++) printf ("%d,",S[j+(n-k-l)*nr]);
+              printf ("%d)\n", S[j+(n-k-l)*nr]);
+#endif
+              k=k+l; /* jump to parent */
+              break;
+            }
+          }
+          found = 1;
+          for (l = 0; l < m; l++){
+            if (S[l + (n-k)*nr] >= S[l + n*nr]){
+              found=0;
+              break;
+            }
+          }
+#if 0
+          printf("find label parent:\n");
+          printf("child =(");
+          for (j=0; j<row; j++) printf ("%d,",S[j+n*nr]);
+          printf ("%d)\n", S[j+n*nr]);
+          printf("parent=(");
+          for (j=0; j<row; j++) printf ("%d,",S[j+(n-k)*nr]);
+          printf ("%d)\n", S[j+(n-k)*nr]);
+          printf("found=%d\n",found);
+#endif
+          /* If all dimension are small, calculate Delta */
+          if (found) {
+            for (l = 0; l<=row; l++){
+              Delta[l] = (l < m-1)? S[l + n*nr]-S[l + (n-k)*nr]: 0;
+            }
+            bad = k;
+            k = n+1;
+            if(detail){
+              printf("S[bad=%ld]=(",(n-bad));
+              for (l=0; l<row; l++) printf ("%d,",S[l+(n-bad)*nr]);
+              printf ("%d)\n", S[row+(n-bad)*nr]);
+            }
+          }else{k=k-1;}
+        }
+      }else if(ver == 220){
+        /***** Version 2.2 *****/
+        /* Calculate m = Uppermost zero at the rightmost sequence */
+        m = row+1; /* row = number of rows + 1 */
+        for(j=0; j<=row; j++){
+          if (S[j + n*nr] == 0) m=j, j=row+1;
+        }
+        /* Determine the bad sequence and calculate Delta */
+        l=getConcestor(S,m,n,nr);
+        if(l>=0){
+          bad=n-l;
+        }else{
+          /* no concestor */
+          bad=0;
+        }
+        /* If all dimension are small, calculate Delta */
+        for(j=0; j<=row; j++){
+          Delta[j] = (j<m-1) ? S[j+n*nr]-S[j+(n-bad)*nr]:0;
         }
       }
     }
@@ -250,7 +426,7 @@ int main(int argc, char *argv[]) {
        for (l=0; l<row; l++) printf ("%d,",Delta[l]);
        printf ("%d)\n", Delta[row]);
         /* Show C matrix */
-        if (ver > 1) {
+        if (ver == 200) {
             printf ("C = ");
             for (l=1; l<=bad; l++) {
                 printf ("(");
@@ -264,7 +440,7 @@ int main(int argc, char *argv[]) {
     }
 
     /* Copy bad sequence */
-    if (ver == 1) {
+    if (ver == 100 || ver == 210 || ver == 220) {
     /***** Version 1 *****/
       while (n < nn) {
         for (l=0; l<=row; l++) S[l + n*nr] = S[l + (n-bad)*nr] + Delta[l];
